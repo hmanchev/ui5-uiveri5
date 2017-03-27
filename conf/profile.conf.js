f8be8d3750a68a069dbe6571fcadc4952b1405ad
Maxim Naidenov <maxim.naidenov@sap.com>
2017-03-27 15:47:14 +0300

    feat: update chromedriver to 2.28

--- conf/profile.conf.js
+++ conf/profile.conf.js
@@ -20,7 +20,7 @@ exports.config = {
           executable: '${filename}-${version}.${patch}.jar'
         },
         chromedriver: {
-          version: '2.25',
+          version: '2.28',
           unzip: true,
           filename: 'chromedriver',
           url: 'https://chromedriver.storage.googleapis.com/${version}/${filename}_${chromeOsTypeString}.zip',
