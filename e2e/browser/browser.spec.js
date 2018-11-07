51baa0ce795fdeb5db5466d73127457c216df059
Naidenov <maxim.naidenov@sap.com>
2018-11-07 14:31:11 +0200

    test: move noui5 test to scenario

--- e2e/browser/browser.spec.js
+++ e2e/browser/browser.spec.js
@@ -1,5 +1,4 @@
 var Runner = require('../Runner');
-var LogInterceptor = require('../LogInterceptor');
 
 describe('Browser tests', function() {
     var app;
@@ -18,18 +17,6 @@ describe('Browser tests', function() {
         return Runner.execTest({
             specs: './browser/specs/by_control.spec.js',
             baseUrl: app.host + '/browser/index.html'
-        });
-    }, 40000);
-
-    it('should fail if no UI5 on page', () => {
-        var logInterceptor = new LogInterceptor();
-        logInterceptor.start('uiveri5 console: DEBUG:.*waitForUI5: no UI5 on this page');
-        return Runner.execTest({
-            specs: './browser/specs/error/noUI5.spec.js',
-            baseUrl: 'http://google.com'
-        }).catch(function () {
-            expect(logInterceptor.aLogs.length).toBeTruthy();
-            logInterceptor.stop();
         });
     }, 40000);
 });
--- e2e/browser/specs/error/noUI5.spec.js
+++
@@ -1,9 +0,0 @@
-/*global describe,it,element,by,expect*/
-
-describe('noUI5', function () {
-	'use strict';
-
-	it('should fail with no UI5 on page', function () {
-        expect(browser.getTitle()).toBe('Google');
-    });
-});
